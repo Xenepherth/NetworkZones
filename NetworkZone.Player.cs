85d39eb79ce79eed7b5862762a8609ef9c8c788b
Özgün Su <ozgunsucontact@gmail.com>
2024-09-02 17:58:51 +0300

    Loading Screen

--- NetworkZone.Player.cs
+++ NetworkZone.Player.cs
@@ -1,21 +1,23 @@
 using UnityEngine;
 using Mirror;
+using Tymski;
 
 public partial class Player
 {
     [Header("Destination")]
+    public SceneReference startingScene;
     public Player player;
     public Vector3 position;
-    
+
 
     [ServerCallback]
     public void OnPortal(SceneLocation scene)
     {
-        
-        Database.singleton.CharacterSave(this, false);
-        Database.SaveCharacterScenePath(this.name, scene.mapScene);
 
-        player.transform.position = scene.position;  
+        Database.singleton.CharacterSave(this, false);
+        Database.singleton.SaveCharacterScenePath(this.name, scene.mapScene);
+
+        player.transform.position = scene.position;
 
         // ask client to switch server
         this.connectionToClient.Send(
